90618cab2c45917c3ef420cf27c644efd7ba262a
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-04-18 02:26:52 +0530

    added url routing to home adn adoption/id

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('<h2>Home View</h2>')
+
+def pet_detail(request, id):
+    return HttpResponse('<h2>pet_detail view with id {}</h2>'.format(id))
