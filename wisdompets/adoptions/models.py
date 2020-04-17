b4d92e83c6af270b8ba9678922f0619ee419e9a0
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-04-18 01:29:09 +0530

    added pet data

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -3,16 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Pet(models.Model):
-    SELECT_GENDER = [('M','Male'),('F','Female')]
+    SEX_CHOICES = [('M','Male'),('F','Female')]
     name = models.CharField(max_length = 30)
     submitter = models.CharField(max_length = 30)
     species = models.CharField(max_length = 30)
     breed = models.CharField(max_length = 30, blank=True)
     description = models.TextField()
-    gender = models.CharField(choices=SELECT_GENDER, max_length = 1, blank=True)
+    sex = models.CharField(choices=SEX_CHOICES, max_length = 1, blank=True)
     submission_date = models.DateTimeField()
     age = models.IntegerField(null=True)
-    vaccination = models.ManyToManyField('Vaccine', blank=True)
+    vaccinations = models.ManyToManyField('Vaccine', blank=True)
 
 class Vaccine(models.Model):
     name = models.CharField(max_length = 30)
