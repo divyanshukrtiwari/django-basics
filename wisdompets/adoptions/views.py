9e8c928be8ca900285262bedfcdba9d54d9ffe27
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2020-04-18 03:03:25 +0530

    added template views

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Pet
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h2>Home View</h2>')
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {'pets': pets})
 
 def pet_detail(request, id):
-    return HttpResponse('<h2>pet_detail view with id {}</h2>'.format(id))
+    try:
+        pet = Pet.objects.get(id=id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet does not exist')
+
+    return render(request, 'pet_detail.html', {'pet': pet})
